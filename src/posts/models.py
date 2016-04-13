9947f7a2cbe4cd37aaf77a379673c0b3a5cba530
KirillUdod <kirilludod@gmail.com>
2016-04-13 12:41:47 +0300

    working with comments

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
@@ -9,6 +10,7 @@ from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
 from markdown_deux import markdown
+from comments.models import Comment
 
 # Create your models here.
 class PostManager(models.Manager):
@@ -57,6 +59,17 @@ class Post(models.Model):
         markdown_text = markdown(content)
         return mark_safe(markdown_text)
 
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
